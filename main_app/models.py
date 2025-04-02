b35f2b63778d8f80b8ad12921a9ef439a673e5af
Arash <arashsaleh798@gmail.com>
2025-04-02 09:38:08 -0700

    delete and update functionality complete

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse 
 
 # Create your models here.
 class Cat(models.Model):
@@ -9,3 +10,8 @@ class Cat(models.Model):
 
     def __str__(self):
         return f'{self.name} ({self.id})'
+    
+        # Define a method to get the URL for this particular cat instance
+    def get_absolute_url(self):
+        # Use the 'reverse' function to dynamically find the URL for viewing this cat's details
+        return reverse('cat-detail', kwargs={'cat_id': self.id})
